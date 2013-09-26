a1f1d233c6b343d220f89589ed0f3a9282ad9366
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 10:44:22 +0800

    Sandbox sample

---
+++ Library/Entity/DownloadActiveReportDataResultEntity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library.Entity
+{
+    [DataContract]
+    public class DownloadActiveReportDataResultEntity
+    {
+        public DownloadActiveReportDataResultEntity() { }
+        /// <summary>
+        /// The page data
+        /// </summary>
+        [DataMember]
+        public List<DownloadActiveReportDataEntity> ARDataList { get; set; }
+        /// <summary>
+        /// Current Total
+        /// </summary>
+        [DataMember]
+        public int CurrentRecordCount { get; set; }
+        /// <summary>
+        /// Next page URL
+        /// </summary>
+        [DataMember]
+        public string NextPageUrl { get; set; }
+        /// <summary>
+        /// Page number
+        /// </summary>
+        [DataMember]
+        public int PageLength { get; set; }
+        /// <summary>
+        /// Current page
+        /// </summary>
+        [DataMember]
+        public int PageNumber { get; set; }
+        /// <summary>
+        /// Page up URL
+        /// </summary>
+        [DataMember]
+        public string PrevPageUrl { get; set; }
+        /// <summary>
+        /// Total
+        /// </summary>
+        [DataMember]
+        public int RecordCount { get; set; }
+        /// <summary>
+        /// 2013 5 15 Distance unit  Kilometer Mile
+        /// </summary>
+        [DataMember]
+        public int DistanceUnit { get; set; }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Before you open run this sample code, you must registor a developer account and get the permision to connect  iHealth. You will receive an email which contains the KEY list when website master approve your request for your application.
+The steps to run the sample:
+1¡¢	Open the solution file ¡¯ DeveloperAPISample.sln¡¯
+2¡¢	Modify the Key list in the ¡®ConnectToiHealthlabs.cs¡¯ file bwtween line 14 to line 21 (in the ¡®Library¡¯ project),
+public string client_id = " your Client ID ";
+public string client_secret = "your Client Secret";
+public string redirect_uri = "Your redirect Uri must contain your redirect domain ";
+public string sc = "your sc";
+public string sv_bp = "your SV of OpenApiBP ";
+public string sv_weight = " your SV of OpenApiWeight ";
+
+3¡¢	Build the solution.
+4¡¢	Begin the web with page ¡®GetStarted.aspx¡¯
